c463512c80f8d7058b703335038fea8e1e0900ad
Manoj Kumar C <manojkumarc2994@gmail.com>
2026-07-13 18:45:35 +0530

    add new change

--- tests/Playwright/UIbasics9_gittest.spec.js
+++ tests/Playwright/UIbasics9_gittest.spec.js
@@ -6,13 +6,8 @@ test.only ("Github testing" ,  async({page}) =>
 {
     await page.goto("https://www.crunchyroll.com/");
     await page.waitForLoadState();
-    await expect(page.locator(".erc-anonymous-consent")).toBeVisible();
-    await page.getByRole("button" , {name: "Please be alert to potential"}).click();
-
-
-
-
-    
+    //await expect(page.locator(".erc-anonymous-consent")).toBeVisible();
+    //await page.getByRole("button" , {name: "Please be alert to potential"}).click();
 
 
 });
